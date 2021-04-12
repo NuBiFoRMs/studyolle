220905202b95478c6ae4972e5e114ec1ae6f5896
NuBiFoRM <nubiform@me.com>
2021-04-12 21:56:37 +0900

    이메일 재전송

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -78,6 +78,8 @@ public class AccountController {
             model.addAttribute("email", account.getEmail());
             return "account/check-email";
         }
+
+        accountService.sendSignUpConfirmEmail(account);
         return "redirect:/";
     }
 }
--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -42,7 +42,7 @@ public class AccountService {
         return newAccount;
     }
 
-    private void sendSignUpConfirmEmail(Account newAccount) {
+    public void sendSignUpConfirmEmail(Account newAccount) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setSubject("스터디올래, 회원 가입 인증");
         mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
