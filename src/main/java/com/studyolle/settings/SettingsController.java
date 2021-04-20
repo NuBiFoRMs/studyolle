203a2c223da2fc2a0f9b9ab487eb96fcf98d3e4c
NuBiFoRM <nubiform@me.com>
2021-04-20 15:50:24 +0900

    apply modelmapper

--- src/main/java/com/studyolle/settings/SettingsController.java
+++ src/main/java/com/studyolle/settings/SettingsController.java
@@ -2,9 +2,9 @@ package com.studyolle.settings;
 
 import com.studyolle.account.AccountService;
 import com.studyolle.account.CurrentUser;
-import com.studyolle.account.SignUpFormValidator;
 import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -31,6 +31,7 @@ public class SettingsController {
 
     private final PasswordFormValidator passwordFormValidator;
     private final AccountService accountService;
+    private final ModelMapper modelMapper;
 
     @InitBinder("passwordForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -40,7 +41,7 @@ public class SettingsController {
     @GetMapping(SETTINGS_PROFILE_URL)
     public String updateProfileForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
-        model.addAttribute(new Profile(account));
+        model.addAttribute(modelMapper.map(account, Profile.class));
         return SETTINGS_PROFILE_VIEW_NAME;
     }
 
@@ -80,7 +81,7 @@ public class SettingsController {
     @GetMapping(SETTINGS_NOTIFICATIONS_URL)
     public String updateNotificationsForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
-        model.addAttribute(new Notifications(account));
+        model.addAttribute(modelMapper.map(account, Notifications.class));
         return SETTINGS_NOTIFICATIONS_VIEW_NAME;
     }
 
